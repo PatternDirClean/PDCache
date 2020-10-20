af7fef9456912f5751b4195375d1ad87d92d8a7a
fybug <1006291762@qq.com>
2020-10-20 15:12:40 +0800

    完善锁缓存工具

--- src/main/java/fybug/nulll/pdcache/supplier/memory/LockMapCache.java
+++ src/main/java/fybug/nulll/pdcache/supplier/memory/LockMapCache.java
@@ -13,35 +13,37 @@ import lombok.experimental.Accessors;
 /**
  * <h2>锁缓存工具.</h2>
  * <p>
- * 基于自填充缓存 {@link SCache} 制作的用于维持一致的并发锁，并在长期不用时释放锁的工具。<br/>
- * 用法与 {@link SCache} 一致，并且内部并发管理指定为 {@link ObjLock}
+ * 基于自填充映射缓存 {@link SMapCache} 制作的用于维持多个锁的一致性，并在长期不用的时候释放锁的工具<br/>
+ * 用法与 {@link SMapCache} 一致，并且内部并发管理指定为 {@link ObjLock}
+ *
+ * @param <K> 锁缓存的键类型
  *
  * @author fybug
  * @version 0.0.1
- * @see SCache
+ * @see SMapCache
  * @see SyLockSupp
  * @since memory 0.0.2
  */
 public
-class LockCache extends SCache<SyLock> {
+class LockMapCache<K> extends SMapCache<K, SyLock> {
     /** 缓存的锁的生成方法 */
     private final Supplier<SyLock> LOCK_SUPP;
 
     /**
      * 构造缓存，指定缓存方式和缓存的锁类型
      *
-     * @see #LockCache(Class, Supplier)
+     * @see #LockMapCache(Class, Supplier)
      */
     public
-    LockCache(@NotNull Class<? extends Reference> refc, @NotNull Class<? extends SyLock> Locktype) {
-        this(refc, new SyLockSupp(Locktype));
-    }
+    LockMapCache(@NotNull Class<? extends Reference> refc, @NotNull Class<? extends SyLock> syLock)
+    { this(refc, new SyLockSupp(syLock)); }
 
     /** 构造缓存，指定缓存方式和缓存的锁的生成方法 */
     public
-    LockCache(@NotNull Class<? extends Reference> refc, @NotNull Supplier<SyLock> lockSupplier) {
+    LockMapCache(@NotNull Class<? extends Reference> refc, @NotNull Supplier<SyLock> syLockSupplier)
+    {
         super(refc, SyLock.newObjLock());
-        LOCK_SUPP = lockSupplier;
+        LOCK_SUPP = syLockSupplier;
     }
 
     /*--------------------------------------------------------------------------------------------*/
@@ -49,20 +51,22 @@ class LockCache extends SCache<SyLock> {
     @NotNull
     @Override
     protected
-    SyLock createData() { return LOCK_SUPP.get(); }
+    SyLock createData(@NotNull K key) { return LOCK_SUPP.get(); }
 
     /*--------------------------------------------------------------------------------------------*/
 
     /**
      * 获取构造工具
      *
+     * @param <K> 缓存的键类型
+     *
      * @return 构造工具
      */
     public static
-    Build build() { return new Build(); }
+    <K> Build<K> build(Class<K> kcla) { return new Build<>(); }
 
     /**
-     * <h2> {@link LockCache} 构造工具.</h2>
+     * <h2> {@link LockMapCache} 构造工具.</h2>
      * <ul>
      * <li>使用 {@link #refernce(Class)} 绑定缓存方式</li>
      * <li>使用 {@link #sylock(Class)} 绑定缓存的锁的类型</li>
@@ -70,12 +74,14 @@ class LockCache extends SCache<SyLock> {
      * <li>使用 {@link #build()} 进行构造</li>
      * </ul>
      *
+     * @param <K> 缓存键类型
+     *
      * @version 0.0.1
-     * @since LockCache 0.0.1
+     * @since LockMapCache 0.0.1
      */
     @Accessors( chain = true, fluent = true )
     public final static
-    class Build {
+    class Build<K> {
         /** 缓存引用类型 */
         @Setter private Class<? extends Reference> refernce = SoftReference.class;
         /** 缓存的锁的生成方法 */
@@ -90,11 +96,12 @@ class LockCache extends SCache<SyLock> {
          */
         @NotNull
         public
-        Build sylock(Class<? extends SyLock> sylock) { return sylock(new SyLockSupp(sylock)); }
+        Build<K> sylock(Class<? extends SyLock> sylock)
+        { return sylock(new SyLockSupp(sylock)); }
 
         /** 设置缓存的锁的生成方法 */
         public
-        Build sylock(@NotNull Supplier<SyLock> syLockSupplier) {
+        Build<K> sylock(@NotNull Supplier<SyLock> syLockSupplier) {
             this.sylock = syLockSupplier;
             return this;
         }
@@ -106,6 +113,6 @@ class LockCache extends SCache<SyLock> {
          */
         @NotNull
         public
-        LockCache build() { return new LockCache(refernce, sylock); }
+        LockMapCache<K> build() { return new LockMapCache<>(refernce, sylock); }
     }
 }
---
+++ src/main/java/fybug/nulll/pdcache/supplier/memory/SyLockSupp.java
@@ -0,0 +1,35 @@
+package fybug.nulll.pdcache.supplier.memory;
+import java.lang.reflect.InvocationTargetException;
+import java.util.function.Supplier;
+
+import fybug.nulll.pdconcurrent.SyLock;
+
+/**
+ * <h2>锁生成接口.</h2>
+ * <p>
+ * 通过构造时传入锁的类，并自动生成锁的实例。<br/>
+ * 发生错误会返回默认的 {@link SyLock#newObjLock()}
+ *
+ * @author fybug
+ * @version 0.0.1
+ * @since memory 0.0.2
+ */
+final
+class SyLockSupp implements Supplier<SyLock> {
+    // 锁的类型
+    private final Class<? extends SyLock> LOCK_CLASS;
+
+    /** 指定锁的类 */
+    SyLockSupp(Class<? extends SyLock> sylock) { LOCK_CLASS = sylock; }
+
+    @Override
+    public
+    SyLock get() {
+        try {
+            return LOCK_CLASS.getConstructor().newInstance();
+        } catch ( InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e ) {
+            e.printStackTrace();
+        }
+        return SyLock.newObjLock();
+    }
+}
