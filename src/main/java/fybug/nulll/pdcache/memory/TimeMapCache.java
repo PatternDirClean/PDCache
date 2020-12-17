0aeb7bc9f54ffb1db2315fd245e9a9693584e85b
fybug <1006291762@qq.com>
2020-12-17 11:22:12 +0800

    时间缓存增加回收接口

--- src/main/java/fybug/nulll/pdcache/memory/TimeMapCache.java
+++ src/main/java/fybug/nulll/pdcache/memory/TimeMapCache.java
@@ -2,23 +2,33 @@ package fybug.nulll.pdcache.memory;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 import fybug.nulll.pdconcurrent.ObjLock;
 import fybug.nulll.pdconcurrent.SyLock;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import lombok.val;
 
 /**
  * <h2>基于时间控制的缓存.</h2>
  * <p>
  * 内部使用 {@link LinkedHashMap} 实现的缓存工具，可指定数据过期的时间（毫秒）。<br/>
- * 数据不会因为当前被持有而不会过期，因为数据基于时间控制，内部不使用 {@link java.lang.ref.Reference} 维护。
+ * 数据不会因为当前被持有而不会过期，因为数据基于时间控制，内部不使用 {@link java.lang.ref.Reference} 维护。<br/>
+ * 数据过期或因为任何原因被移除都会触发回收事件，在 {@link #putData(Object, Object, long, Consumer)} 最后一个参数指定，接口传入当前数据。<br/>
+ * 回收事件执行时在一个线程池内执行，可使用 {@link #setGcThread(ExecutorService)} 指定，默认为 {@link Executors#newCachedThreadPool()}
+ * <br/><br/>
+ * <b>注意：{@link #clear()} 不会触发回收事件，因为这是缓存清空操作而不是单独回收数据。</b>
  * <br/><br/>
  * 内部会使用线程检查当前在热度排序中最后的几个数据，过期则会被主动移除。<br/>
  * 可通过 {@link #closeTimeTask()} 关闭扫描线程，效果等同于构造时传入的扫描间隔为 0，在不需要该工具时也可以使用 {@link #closeTimeTaskAndClear()} 清空数据并关闭扫描线程。<br/>
@@ -34,7 +44,7 @@ import lombok.experimental.Accessors;
  * </ul>
  *
  * @author fybug
- * @version 0.0.1
+ * @version 0.0.2
  * @since memory 0.0.2
  */
 public
@@ -51,6 +61,16 @@ class TimeMapCache<K, V> {
     @Nullable protected final Timer timerRun;
     /** 从尾部扫描的数量 */
     @Setter protected volatile int scarrenNum;
+
+    /**
+     * 回收事件执行线程池
+     *
+     * @since 0.0.2
+     */
+    @NonNull
+    @Setter
+    @Getter
+    protected ExecutorService GcThread = Executors.newCachedThreadPool();
 
     //----------------------------------------------------------------------------------------------
 
@@ -91,7 +111,7 @@ class TimeMapCache<K, V> {
      * 缓存检查任务
      *
      * @author fybug
-     * @version 0.0.1
+     * @version 0.0.2
      * @since TimeMapCache 0.0.1
      */
     protected final
@@ -102,14 +122,10 @@ class TimeMapCache<K, V> {
             LOCK.write(() -> {
                 var now = System.currentTimeMillis();
                 var i = 0;
-
                 var iter = map.entrySet().iterator();
-                Map.Entry<K, Enty<V>> v;
-                while( iter.hasNext() && i++ < scarrenNum ){
-                    v = iter.next();
-                    if (v.getValue().maxtime <= now)
-                        iter.remove();
-                }
+                // 检查
+                while( iter.hasNext() && i++ < scarrenNum )
+                    CheckVal(iter, now);
             });
         }
     }
@@ -136,7 +152,21 @@ class TimeMapCache<K, V> {
      */
     public
     void putData(@NotNull K k, @Nullable V v, long datatime)
-    { LOCK.write(() -> map.put(k, new Enty<>(System.currentTimeMillis() + datatime, v))); }
+    { putData(k, v, datatime, null); }
+
+    /**
+     * 放入数据
+     *
+     * @param k        数据的键
+     * @param v        数据内容
+     * @param datatime 数据的过期时间，相对于当前
+     * @param endrun   数据被回收事件回调，接口传入当前数据，为空则不触发
+     *
+     * @since 0.0.2
+     */
+    public
+    void putData(@NotNull K k, @Nullable V v, long datatime, @Nullable Consumer<V> endrun)
+    { LOCK.write(() -> map.put(k, new Enty<>(System.currentTimeMillis() + datatime, v, endrun))); }
 
     //-------------------------------------
 
@@ -146,9 +176,13 @@ class TimeMapCache<K, V> {
      * @param k 数据的键
      */
     public
-    void removeData(@NotNull K k) { LOCK.write(() -> map.remove(k)); }
+    void removeData(@NotNull K k) { LOCK.write(() -> GcVal(map.remove(k))); }
 
-    /** 清除所有数据 */
+    /**
+     * 清除所有数据
+     * <p>
+     * <b>不会触发回收事件</b>
+     */
     public
     void clear() { LOCK.write(map::clear); }
 
@@ -189,7 +223,7 @@ class TimeMapCache<K, V> {
                 map.put(k, v);
                 return v.val;
             }
-            map.remove(k);
+            GcVal(map.remove(k));
             return null;
         });
     }
@@ -207,7 +241,7 @@ class TimeMapCache<K, V> {
     /** 检查数据是否可用 */
     protected
     boolean check(@NotNull K k)
-    { return map.getOrDefault(k, new Enty<>(0, null)).maxtime > System.currentTimeMillis(); }
+    { return map.getOrDefault(k, new Enty<>(0, null, null)).maxtime > System.currentTimeMillis(); }
 
     //-------------------------------------
 
@@ -220,8 +254,8 @@ class TimeMapCache<K, V> {
      */
     public
     long dataHasTime(@NotNull K k) {
-        return LOCK.read(() -> Math.max(
-                map.getOrDefault(k, new Enty<>(0, null)).maxtime - System.currentTimeMillis(), 0));
+        return LOCK.read(() -> Math.max(map.getOrDefault(k, new Enty<>(0, null, null)).maxtime -
+                                        System.currentTimeMillis(), 0));
     }
 
     /**
@@ -234,12 +268,9 @@ class TimeMapCache<K, V> {
         LOCK.write(() -> {
             long nowtime = System.currentTimeMillis();
             var iter = map.entrySet().iterator();
-            Map.Entry<K, Enty<V>> v;
-            while( iter.hasNext() ){
-                v = iter.next();
-                if (v.getValue().maxtime <= nowtime)
-                    iter.remove();
-            }
+            // 检查
+            while( iter.hasNext() )
+                CheckVal(iter, nowtime);
         });
     }
 
@@ -260,6 +291,46 @@ class TimeMapCache<K, V> {
     void closeTimeTaskAndClear() {
         closeTimeTask();
         clear();
+        getGcThread().shutdown();
+    }
+
+    /*--------------------------------------------------------------------------------------------*/
+
+    /**
+     * 检查并移除已经过期的数据
+     *
+     * @since 0.0.2
+     */
+    private
+    void CheckVal(Iterator<Map.Entry<K, Enty<V>>> iter, long nowtime) {
+        // 当前节点
+        var v = iter.next();
+        var val = v.getValue();
+        // 检查是否最大时间
+        if (val.maxtime <= nowtime) {
+            // 移除
+            iter.remove();
+            // 异步执行回收事件
+            GcVal(val);
+        }
+    }
+
+    /**
+     * 申请执行当前数据的回收事件
+     *
+     * @since 0.0.2
+     */
+    private
+    void GcVal(Enty<V> v) {
+        if (v == null)
+            return;
+        // 当前回收事件
+        val endrun = v.getEndrun();
+        // 线程池执行
+        getGcThread().submit(() -> {
+            if (endrun != null)
+                endrun.accept(v.getVal());
+        });
     }
 
     /*--------------------------------------------------------------------------------------------*/
@@ -317,7 +388,7 @@ class TimeMapCache<K, V> {
      * <h2>数据记录对象.</h2>
      *
      * @author fybug
-     * @version 0.0.1
+     * @version 0.0.2
      * @since TimeMapCache 0.0.1
      */
     @AllArgsConstructor
@@ -329,5 +400,13 @@ class TimeMapCache<K, V> {
         public long maxtime;
         /** 数据 */
         @Nullable public V val;
+        /**
+         * 回收时的处理
+         * <p>
+         * 接口传入当前数据
+         *
+         * @since 0.0.2
+         */
+        @Nullable public Consumer<V> endrun;
     }
 }
